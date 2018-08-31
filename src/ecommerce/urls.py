45f7921c7e3bcdf4fe8f0813584a3d3e0b1bfdee
dalaisubi <suvendu.dalai2@gmail.com>
2018-09-01 04:21:21 +0530

    styling in navbar and worked using template filter and forloop.counter

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -33,12 +33,12 @@ from .views import home_page, about_page, contact_page, login_page, register_pag
     
 
 urlpatterns = [
-	path('', home_page),
-	path('about/', about_page),
-	path('contact', contact_page),
+	path('', home_page, name='home'),
+	path('about/', about_page, name='about'),
+	path('contact', contact_page, name='contact'),
     path('admin/', admin.site.urls),
-    path('login/', login_page),
-    path('register/', register_page),
+    path('login/', login_page, name='login'),
+    path('register/', register_page, name='register'),
 
     path('products/', include('products.urls', namespace='products')),
     # path('products',ProductListView.as_view()),
