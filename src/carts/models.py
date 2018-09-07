24f7de219464217c362fa34c67128ae5367d290f
dalaisubi <suvendu.dalai2@gmail.com>
2018-09-08 03:11:50 +0530

    Associating user once he login using extended Model Manger

--- src/carts/models.py
+++ src/carts/models.py
@@ -4,6 +4,13 @@ from products.models import Product
 
 User = settings.AUTH_USER_MODEL
 # Create your models here.
+class CartManger(models.Manager):
+	def new(self, user=None):
+		user_obj = None
+		if user is not None:
+			if user.is_authenticated:
+				user_obj = user
+		return self.create(user=user_obj)
 
 class Cart(models.Model):
 	user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
@@ -11,6 +18,8 @@ class Cart(models.Model):
 	total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
+
+	objects = CartManger()
 
 	def __str__(self):
 		return str(self.id)
--- src/carts/views.py
+++ src/carts/views.py
@@ -7,19 +7,15 @@ def cart_create(user=None):
 	return cart_obj
 def cart_home(request):
 	#del request.session['cart_id']
-	request.session['cart_id'] = "12"
 	cart_id = request.session.get('cart_id', None)
-	# if cart_id is None: # and isinstance(cart_id,int):
-		
-	# 	cart_obj = cart_create()
-	# 	request.session['cart_id'] = cart_obj.id
-	# else: 
 	qs = Cart.objects.filter(id=cart_id)
 	if qs.count() == 1:
-		print("Cart ID exists")
 		cart_obj = qs.first()
+		if request.user.is_authenticated and cart_obj.user is None:
+			cart_obj.user = request.user
+			cart_obj.save()
 	else:
-		cart_obj = cart_create()
+		cart_obj = Cart.objects.new(user=request.user)
 		request.session['cart_id'] = cart_obj.id		
 
 	return render(request, 'carts/home.html', {})
