afb94588ad8b83f42f3209f064d1abbee3b61072
dalaisubi <suvendu.dalai2@gmail.com>
2018-09-08 04:52:47 +0530

    m2m_changed signal to calculate cart total

--- src/carts/views.py
+++ src/carts/views.py
@@ -7,4 +7,13 @@ def cart_create(user=None):
 	return cart_obj
 def cart_home(request):
 	cart_obj, new_obj = Cart.objects.new_or_get(request)
+
+	# products = cart_obj.products.all()
+	# total = 0
+	# print(new_obj, cart_obj, products)
+	# for price in products:
+	# 	total += price.price
+	# print(total)
+	# cart_obj.total = total	
+	# cart_obj.save()
 	return render(request, 'carts/home.html', {})
