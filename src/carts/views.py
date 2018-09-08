1a1831e28a6a587a122de87ed4f4cac9e39b1f90
dalaisubi <suvendu.dalai2@gmail.com>
2018-09-08 06:55:18 +0530

    Add to Cart Form added

--- src/carts/views.py
+++ src/carts/views.py
@@ -5,24 +5,22 @@ from products.models import Product
 def cart_home(request):
 	cart_obj, new_obj = Cart.objects.new_or_get(request)
 
-	# products = cart_obj.products.all()
-	# total = 0
-	# print(new_obj, cart_obj, products)
-	# for price in products:
-	# 	total += price.price
-	# print(total)
-	# cart_obj.total = total	
-	# cart_obj.save()
 	return render(request, 'carts/home.html', {})
 
 
 def cart_update(request):
-	product_id = 1
-	product_obj = Product.objects.get(id=product_id)
-	cart_obj, new_obj = Cart.objects.new_or_get(request)
-	if product_obj in cart_obj.products.all():
-		cart_obj.products.remove(product_obj)
-	else:
-		cart_obj.products.add(product_obj)		
+	product_id = request.POST.get('product_id')
+	if product_id is not None:
+		try:
+			product_obj = Product.objects.get(id=product_id)
+		except Product.DoesNotExist:
+			print("Show message to user, product i gone?")
+			return redirect("cart:home")
+		cart_obj, new_obj = Cart.objects.new_or_get(request)
+		if product_obj in cart_obj.products.all():
+			cart_obj.products.remove(product_obj)
+		else:
+			cart_obj.products.add(product_obj)	
+		return redirect("cart:home")		
 	#return redirect(product_obj.get_absolute_url())
-	return redirect("cart:home")
+	
