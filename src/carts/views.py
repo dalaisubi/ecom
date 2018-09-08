18ddac15cae28c38c23785568cbe3e747a12f298
dalaisubi <suvendu.dalai2@gmail.com>
2018-09-08 07:18:38 +0530

    Display cart items

--- src/carts/views.py
+++ src/carts/views.py
@@ -5,7 +5,7 @@ from products.models import Product
 def cart_home(request):
 	cart_obj, new_obj = Cart.objects.new_or_get(request)
 
-	return render(request, 'carts/home.html', {})
+	return render(request, 'carts/home.html', {"cart": cart_obj})
 
 
 def cart_update(request):
