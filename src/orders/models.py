457bbe8c8dafdcf2a26e6ecaababe43917cbcae4
dalaisubi <suvendu.dalai2@gmail.com>
2018-09-26 04:31:42 +0530

    Finalize Checkout

--- src/orders/models.py
+++ src/orders/models.py
@@ -54,6 +54,21 @@ class Order(models.Model):
 		self.save()
 		return new_total
 
+	def check_done(self):
+		billing_profile = self.billing_profile
+		shipping_address = self.shipping_address
+		billing_address = self.billing_address
+		total = self.total
+		if billing_profile and shipping_address and billing_address and total > 0:
+			return True	
+		return False
+		
+	def mark_paid(self):
+		if self.check_done():
+			self.status = 'paid'
+			self.save()
+		return self.status			
+
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
 	if not instance.order_id:
 		instance.order_id = unique_order_id_generator(instance)
